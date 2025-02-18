8959d8593e7c6b0fcf37ac8f74c334dbd4dfb57e
javier <javier@example.com>
2025-02-18 18:27:00 +0100

    a

--- src/Proyecto/WesternMoon.java
+++ src/Proyecto/WesternMoon.java
@@ -4,11 +4,31 @@ public class WesternMoon {
 
     public static void main(String[] args) {
 
+        // Creación del escaner
         Scanner sc = new Scanner(System.in);
 
+        // Solicitud de datos al usuario
         System.out.println("Introduzca el número esperado de alienígenas: ");
         int numEsperadoAliens = sc.nextInt();
-        System.out.println("");
+        System.out.println("Introduzca la distancia en años luz de la nave DKW-RR.3: ");
+        float distanciaAñosLuz = sc.nextFloat();
+        System.out.println("Se va realizar un listado de soldados, introduzca la cantidad de soldados: ");
+        int numSoldados = sc.nextInt();
+        String[] listaSoldatos = new String[numSoldados];
+        for (int x = 0; x < numSoldados; x++) {
+            System.out.println("Introduzca el nombre del soldado número " + (x + 1));
+            listaSoldatos[x] = sc.nextLine();
+        }
+        System.out.println("Se va a realizar un listado de mineros, introduzca la cantidad de mineros: ");
+        int numMineros = sc.nextInt();
+        String[] listaMineros = new String[numMineros];
+        for (int y = 0; y < numMineros; y++) {
+            System.out.println("Introduzca el nombre del minero número " + (y + 1) + ": ");
+            listaMineros[y] = sc.nextLine();
+        }
+        System.out.println("Introduzca el número de aerocars a utilizar: ");
+        int numAerocarsUtilizar = sc.nextInt();
+        
 
     }
 
