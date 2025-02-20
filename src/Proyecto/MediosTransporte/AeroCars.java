a49c65bcbd61a1835e6ee0f6e6b148346298e786
javier <javier@example.com>
2025-02-20 16:18:49 +0100

    Actualizamos la rama con master 20/02 16:18

--- src/Proyecto/MediosTransporte/AeroCars.java
+++ src/Proyecto/MediosTransporte/AeroCars.java
@@ -1,3 +1,16 @@
-public class AeroCars extends MedioTransporte{
-	
+public class AeroCars extends MedioTransporte {
+    int numPropulsores;
+    int caballosDePotencia;
+    boolean esDeCombate;
+
+    public AeroCars() {
+    }
+
+    public AeroCars(int numPlazas, String tipoCombustible, int maxCombustible, int kilometraje,
+            ResponsableVehiculo rVehiculo, int numPropulsores, int caballosDePotencia, boolean esDeCombate) {
+        super(numPlazas, tipoCombustible, maxCombustible, kilometraje, rVehiculo);
+        this.numPropulsores = numPropulsores;
+        this.caballosDePotencia = caballosDePotencia;
+        this.esDeCombate = esDeCombate;
+    }
 }
