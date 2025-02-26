cea4fb8e5cf61374957c297f8413fa9cd4ac9c54
¿Unknown? <110683807+44k1@users.noreply.github.com>
2025-02-26 19:33:37 +0100

    Añadidos metodos y atributos particulares

--- src/Proyecto/MediosTransporte/AeroCars.java
+++ src/Proyecto/MediosTransporte/AeroCars.java
@@ -1,13 +1,25 @@
 public class AeroCars extends MedioTransporte{
-    static int contador=0;
     int numPropulsores;
     int caballosDePotencia;
     boolean esDeCombate;
+    final Float precioYurs = (float) 12;
     public AeroCars(){}
+
     public AeroCars(String nombre,int numPlazas,String tipoCombustible, int maxCombustible,int kilometraje,ResponsableVehiculo rVehiculo,int numPropulsores,int caballosDePotencia,boolean esDeCombate){
         super(nombre,numPlazas, tipoCombustible,  maxCombustible, kilometraje,rVehiculo);
         this.numPropulsores=numPropulsores;
         this.caballosDePotencia=caballosDePotencia;
         this.esDeCombate=esDeCombate;
+    }
+    public Float calculaCarburante(Float distanciaAñosLuz){
+        //Funcion para realizar la raiz cuadrada
+        float calculo= (float) Math.sqrt(distanciaAñosLuz) * precioYurs;
+        return calculo;
+    }
+
+    public String metodoTransmision(Float distanciaAñosLuz){
+        if(distanciaAñosLuz < 1){
+            return "Economico";
+        }else return "Estelar";
     }
 }
