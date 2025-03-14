bd65e01b83a8b0d77d2b7c89d0a180b4d585f5a5
javier <javier@example.com>
2025-03-14 16:51:04 +0100

    TurboJects actualizado

--- src/Proyecto/MediosTransporte/TurboJets.java
+++ src/Proyecto/MediosTransporte/TurboJets.java
@@ -1,3 +1,43 @@
-public class TurboJets extends MedioTransporte{
+public class TurboJets extends MedioTransporte {
+
+    // Atributos
+    int numAlas;
+    float caballosDePotencia;
+    boolean esDeCombate;
+
+    // Constructores
+    public TurboJets() {
+    }
+
+    public TurboJets(int numAlas, float caballosDePotencia, boolean esDeCombate) {
+        this.numAlas = numAlas;
+        this.caballosDePotencia = caballosDePotencia;
+        this.esDeCombate = esDeCombate;
+    }
+
+    // Getters y Setters
+    public int getNumAlas() {
+        return numAlas;
+    }
+
+    public float getCaballosDePotencia() {
+        return caballosDePotencia;
+    }
+
+    public boolean getEsDeCombate() {
+        return esDeCombate;
+    }
+
+    public void setNumAlas(int numAlas) {
+        this.numAlas = numAlas;
+    }
+
+    public void setCaballosDePotencia(float caballosDePotencia) {
+        this.caballosDePotencia = caballosDePotencia;
+    }
+
+    public void setEsDeCombate(boolean esDeCombate) {
+        this.esDeCombate = esDeCombate;
+    }
 
 }
